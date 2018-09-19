a01c2ce575eae39a597040e01611fe56d0104d8e
Kelvin Gu <jgu55@its.jnj.com>
2018-09-19 16:13:32 +0800

    add data computed filter

--- vue/todo/src/App.vue
+++ vue/todo/src/App.vue
@@ -1,8 +1,28 @@
 <template>
 	<div id="app">
-		<h4>
-			To Do List
+		<h4 class="bg-primary text-white text-center p-2">
+			{{name}}'s To Do List
 		</h4>
+		<div class="container-fluid p-4">
+			<div class="row">
+				<div class="col font-weight-bold">Task</div>
+				<div class="col-2 font-weight-bold">Done</div>
+			</div>
+			<div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
+				<div class="col">{{t.action}}</div>
+				<div class="col-2 text-center">
+					<input type="checkbox" v-model="t.done" class="form-check-input" />
+				</div>
+			</div>
+			<div class="row bg-secondary py-2 mt-2 text-white">
+				<div class="col text-center">
+					<input type="checkbox" v-model="hideCompleted" class="form-check-input" />
+					<label class="form-check-label font-weight-bold">
+						Hide completed tasks
+					</label>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -10,7 +30,23 @@
 	//import HelloWorld from './components/HelloWorld.vue'
 
 	export default {
-		name: 'app'
+		name: 'app',
+		data() {
+			return {
+				name: "KG",
+				tasks: [{action: "Buy Flowers", done: false},
+						{action: "Get Shoes", done: false},
+						{action: "Collect Tickets", done: true},
+						{action: "Call Joe", done: false}],
+				hideCompleted: true
+			}
+		},
+		computed: {
+			filteredTasks() {
+				return this.hideCompleted ?
+					this.tasks.filter(t => !t.done) : this.tasks
+			}
+		}
 	}
 </script>
 
