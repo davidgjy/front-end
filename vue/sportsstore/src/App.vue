3de311d41cd791ebbe8e987fc0eaeb29a3a56fc6
KG <davidgjy@163.com>
2020-09-15 16:48:34 +0800

    update app.vue

--- vue/sportsstore/src/App.vue
+++ vue/sportsstore/src/App.vue
@@ -2,9 +2,17 @@
   <store />
 </template>
 <script>
-	import Store from "./components/Store";
-	export default {
-		name: 'app',
-		components: { Store }
-	}
+import Store from "./components/Store";
+import { mapActions } from "vuex";
+
+export default {
+  name: "app",
+  components: { Store },
+  methods: {
+    ...mapActions(["getData"]),
+  },
+  created() {
+    this.getData();
+  }
+};
 </script>
