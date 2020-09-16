dbe4c2474ad199c6a24ec104cd72840cc0388bd7
KG <davidgjy@163.com>
2020-09-16 22:45:55 +0800

    add shopping cart

--- vue/sportsstore/src/components/ProductList.vue
+++ vue/sportsstore/src/components/ProductList.vue
@@ -7,25 +7,39 @@
 					{{ p.price | currency }}
 				</span>
 			</h4>
-			<div class="card-text bg-white p-1">{{ p.description }}</div>
+			<div class="card-text bg-white p-1">
+				{{ p.description }}
+				<button class="btn btn-success btn-sm float-right" v-on:click="handleProductAdd(p)">
+                    Add To Cart
+                </button>
+			</div>
 		</div>
 		<page-controls/>
 	</div>
 </template>
 <script>
-	import {mapGetters} from "vuex";
-	import PageControls from "./PageControls";
+import { mapGetters, mapMutations } from "vuex";
+import PageControls from "./PageControls";
 
-	export default {
-		components: {PageControls},
-		computed: {
-			...mapGetters({products: "processedProducts"})
-		},
-		filters: {
-			currency(value) {
-				return new Intl.NumberFormat("en-US",
-					{style: "currency", currency: "USD"}).format(value);
-			}
-		}
-	}
+export default {
+  components: { PageControls },
+  computed: {
+    ...mapGetters({ products: "processedProducts" }),
+  },
+  filters: {
+    currency(value) {
+      return new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(value);
+    },
+  },
+  methods: {
+    ...mapMutations({ addProduct: "cart/addProduct" }),
+    handleProductAdd(product) {
+      this.addProduct(product);
+      this.$router.push("/cart");
+    },
+  }
+};
 </script>
